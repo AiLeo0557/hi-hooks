2f27400ef01e0770fcacfdb04b6e9bda81f5dcae
AiLeo0557 <leo1949@dingtalk.com>
2025-04-16 10:26:13 +0800

    feat: 添加 useToggle 函数以实现状态切换功能

---
+++ src/useToggle.ts
@@ -0,0 +1,19 @@
+import useState from "./useState"
+
+/**
+ * @function 状态转换
+ * @param key 状态名称唯一标识
+ * @param defaultValue
+ * @returns
+ */
+export default function useToggle(...args: any) {
+  const state = useState(...args)
+  function onToggle(value?: boolean) {
+    if (typeof value === 'boolean') {
+      state.value = value
+      return
+    }
+    state.value = !state.value
+  }
+  return [state, onToggle]
+}
